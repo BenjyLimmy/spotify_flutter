5289c92d7880eba9fa408ed24b7a3dec94f03cd5
BenjyLimmy <126404899+BenjyLimmy@users.noreply.github.com>
2024-11-10 15:44:24 +0800

    Remove debug banner

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:client/core/theme/theme.dart';
 import 'package:client/features/auth/view/pages/login_page.dart';
-import 'package:client/features/auth/view/pages/signup_page.dart';
+// import 'package:client/features/auth/view/pages/signup_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: AppTheme.darkThemeMode,
       home: const LoginPage(),
     );
