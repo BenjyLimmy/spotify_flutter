4f6e649d11adf0d83c1a207592bcfccb3d578105
BenjyLimmy <126404899+BenjyLimmy@users.noreply.github.com>
2024-11-13 08:59:49 +0800

    Generated dataclass for User Model (client side)

--- client/lib/features/auth/model/user_model.dart
+++ client/lib/features/auth/model/user_model.dart
@@ -1,4 +1,6 @@
-// User data class
+import 'dart:convert';
+
+// User data class generated using 'Dart Data Class Generator Extension'
 class UserModel {
   final String name;
   final String email;
@@ -9,4 +11,52 @@ class UserModel {
     required this.email,
     required this.id,
   });
+
+  UserModel copyWith({
+    String? name,
+    String? email,
+    String? id,
+  }) {
+    return UserModel(
+      name: name ?? this.name,
+      email: email ?? this.email,
+      id: id ?? this.id,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'name': name,
+      'email': email,
+      'id': id,
+    };
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      name: map['name'] ?? '',
+      email: map['email'] ?? '',
+      id: map['id'] ?? '',
+    );
+  }
+
+  // UserModel -> JSON
+  String toJson() => json.encode(toMap());
+
+  // JSON -> UserModel
+  factory UserModel.fromJson(String source) =>
+      UserModel.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() => 'UserModel(name: $name, email: $email, id: $id)';
+
+  @override
+  bool operator ==(covariant UserModel other) {
+    if (identical(this, other)) return true;
+
+    return other.name == name && other.email == email && other.id == id;
+  }
+
+  @override
+  int get hashCode => name.hashCode ^ email.hashCode ^ id.hashCode;
 }
