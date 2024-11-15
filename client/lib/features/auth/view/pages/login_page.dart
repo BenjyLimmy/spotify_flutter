76b8135665dd3d705c72b9b7b81d37d39a20f6e6
BenjyLimmy <126404899+BenjyLimmy@users.noreply.github.com>
2024-11-16 01:18:08 +0800

    Added login logic for view observing authViewModelProvider

--- client/lib/features/auth/view/pages/login_page.dart
+++ client/lib/features/auth/view/pages/login_page.dart
@@ -1,20 +1,23 @@
 import 'package:client/core/theme/app_palette.dart';
+import 'package:client/core/widgets/loader.dart';
 import 'package:client/features/auth/repositories/auth_remote_repository.dart';
 import 'package:client/features/auth/view/pages/signup_page.dart';
 import 'package:client/features/auth/view/widgets/auth_gradient_button.dart';
 import 'package:client/features/auth/view/widgets/custom_field.dart';
+import 'package:client/features/auth/viewmodel/auth_viewmodel.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:gap/gap.dart';
 
-class LoginPage extends StatefulWidget {
+class LoginPage extends ConsumerStatefulWidget {
   const LoginPage({super.key});
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  ConsumerState<LoginPage> createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _LoginPageState extends ConsumerState<LoginPage> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
@@ -28,80 +31,103 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
+    final isLoading = ref.watch(authViewModelProvider)?.isLoading == true;
+
+    ref.listen(
+      authViewModelProvider,
+      (_, next) {
+        next?.when(
+          // When there is data (UserModel)
+          data: (data) {
+            // TODO: Navigate to home page
+            // Navigator.push(
+            //   context,
+            //   MaterialPageRoute(
+            //     builder: (context) => const LoginPage(),
+            //   ),
+            // );
+          },
+          error: (error, st) {},
+          loading: () {},
+        );
+      },
+    );
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Form(
-          //call validator method inside children which are TextFormFields
-          key: formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Sign In.',
-                style: TextStyle(
-                  fontSize: 60,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const Gap(30),
-              CustomField(
-                hintText: 'Email',
-                controller: emailController,
-              ),
-              const Gap(15),
-              CustomField(
-                hintText: 'Password',
-                controller: passwordController,
-                isObscureText: true,
-              ),
-              const Gap(20),
-              AuthGradientButton(
-                buttonText: 'Sign In',
-                onTap: () async {
-                  final res = await AuthRemoteRepository().login(
-                    email: emailController.text,
-                    password: passwordController.text,
-                  );
-
-                  // check if success (Right) / failure (Left)
-                  final val = switch (res) {
-                    Left(value: final l) => l,
-                    Right(value: final r) => r,
-                  };
-                  print(val);
-                },
-              ),
-              const Gap(20),
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => SignupPage(),
-                    ),
-                  );
-                },
-                child: RichText(
-                  text: TextSpan(
-                    text: 'Don\'t have an account? ',
-                    style: Theme.of(context).textTheme.titleMedium,
-                    children: const [
-                      TextSpan(
-                        text: 'Sign Up',
-                        style: TextStyle(
-                            color: Palette.gradient2,
-                            fontWeight: FontWeight.bold),
+      body: isLoading
+          ? const Loader()
+          : Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Form(
+                //call validator method inside children which are TextFormFields
+                key: formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Sign In.',
+                      style: TextStyle(
+                        fontSize: 60,
+                        fontWeight: FontWeight.bold,
                       ),
-                    ],
-                  ),
+                    ),
+                    const Gap(30),
+                    CustomField(
+                      hintText: 'Email',
+                      controller: emailController,
+                    ),
+                    const Gap(15),
+                    CustomField(
+                      hintText: 'Password',
+                      controller: passwordController,
+                      isObscureText: true,
+                    ),
+                    const Gap(20),
+                    AuthGradientButton(
+                      buttonText: 'Sign In',
+                      onTap: () async {
+                        final res = await AuthRemoteRepository().login(
+                          email: emailController.text,
+                          password: passwordController.text,
+                        );
+
+                        // check if success (Right) / failure (Left)
+                        final val = switch (res) {
+                          Left(value: final l) => l,
+                          Right(value: final r) => r,
+                        };
+                        print(val);
+                      },
+                    ),
+                    const Gap(20),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => SignupPage(),
+                          ),
+                        );
+                      },
+                      child: RichText(
+                        text: TextSpan(
+                          text: 'Don\'t have an account? ',
+                          style: Theme.of(context).textTheme.titleMedium,
+                          children: const [
+                            TextSpan(
+                              text: 'Sign Up',
+                              style: TextStyle(
+                                  color: Palette.gradient2,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
                 ),
-              )
-            ],
-          ),
-        ),
-      ),
+              ),
+            ),
     );
   }
 }
