139028b0e493ac5850b013b9b20d6612b74915c0
BenjyLimmy <126404899+BenjyLimmy@users.noreply.github.com>
2024-11-16 14:57:35 +0800

    Add AuthLocalRepository for token storage using SharedPreferences

--- client/lib/features/auth/repositories/auth_local_repository.dart
+++ client/lib/features/auth/repositories/auth_local_repository.dart
@@ -1,2 +1,20 @@
 // HTTP calls interacting with local database (phone)
-class AuthLocalRepository {}
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AuthLocalRepository {
+  late SharedPreferences _sharedPreferences;
+
+  Future<void> init() async {
+    _sharedPreferences = await SharedPreferences.getInstance();
+  }
+
+  void setToken(String? token) {
+    if (token != null) {
+      _sharedPreferences.setString('x-auth-token', token);
+    }
+  }
+
+  String? getToken() {
+    return _sharedPreferences.getString('x-auth-token');
+  }
+}
