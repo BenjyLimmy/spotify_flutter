8ffc680d2ed3d692c2973c8f21948d5ed1c31ed5
BenjyLimmy <126404899+BenjyLimmy@users.noreply.github.com>
2024-11-16 11:51:29 +0800

    Implement JWT encoding with RS256 algorithm

--- server/routes/auth.py
+++ server/routes/auth.py
@@ -6,8 +6,17 @@ from database import get_db
 from models.user import User
 from pydantic_schemas.user_create import UserCreate
 from sqlalchemy.orm import Session
-
+import jwt
 from pydantic_schemas.user_login import UserLogin
+from dotenv import load_dotenv
+import os
+
+# Load .env file
+load_dotenv()
+
+# Retrieve the keys
+private_key = os.getenv("PRIVATE_KEY").replace('\\n', '\n')  # Handle single-line keys
+public_key = os.getenv("PUBLIC_KEY").replace('\\n', '\n')
 
 router = APIRouter()
 
@@ -50,8 +59,12 @@ def login_user(user: UserLogin, db: Session = Depends(get_db)):
     
     # If password match, return User instance
     if is_match:
-        return user_db
+        token = jwt.encode({'id': user_db.id}, private_key, algorithm='RS256')
+        
+        return {'token': token, 'user': user_db}
     # Throw error for incorrect password
     else:
         raise HTTPException(400, 'Incorrect password.')
+    
+
     
