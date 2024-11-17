634abd0557e503e11d844f01b52c141bc007a41f
BenjyLimmy <126404899+BenjyLimmy@users.noreply.github.com>
2024-11-17 14:40:22 +0800

    Add comments

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -7,13 +7,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
+  // Ensure Flutter bindings are initialized before calling any asynchronous code
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Create a provider container for managing state and dependencies
   final container = ProviderContainer();
+
+  // Read the AuthViewModel notifier from the container
   final AuthViewModel notifier = container.read(authViewModelProvider.notifier);
+
+  // Initialize shared preferences and fetch user data asynchronously
   await notifier.initSharedPreferences();
   await notifier.getUserData();
+
+  // Run the app with an uncontrolled provider scope to manage the container
   runApp(UncontrolledProviderScope(
-    container: container,
+    container: container, // Pass the container to the app's widget tree
     child: const MyApp(),
   ));
 }
@@ -23,12 +32,14 @@ class MyApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    // Watch the currentUserNotifierProvider to get the current user state
     final currentUser = ref.watch(currentUserNotifierProvider);
 
     return MaterialApp(
       title: 'Spotify Clone',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.darkThemeMode,
+      // Conditionally display the SignupPage or HomePage based on whether a user is logged in
       home: currentUser == null ? const SignupPage() : const HomePage(),
     );
   }
