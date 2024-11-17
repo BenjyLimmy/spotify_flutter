b94741c7c08f49cac9b1fef8674b5682260910cc
BenjyLimmy <126404899+BenjyLimmy@users.noreply.github.com>
2024-11-17 17:56:34 +0800

    feat: Added placeholder UI for uploading songs

--- client/lib/features/home/view/pages/upload_song_page.dart
+++ client/lib/features/home/view/pages/upload_song_page.dart
@@ -1,5 +1,7 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gap/gap.dart';
 
 class UploadSongPage extends ConsumerStatefulWidget {
   const UploadSongPage({super.key});
@@ -11,8 +13,39 @@ class UploadSongPage extends ConsumerStatefulWidget {
 class _UploadSongPageState extends ConsumerState<UploadSongPage> {
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-      appBar: AppBar(title: const Text('Upload Song')),
+      appBar: AppBar(
+        title: const Center(
+          child: Text('Upload Song'),
+        ),
+      ),
+      body: Column(
+        children: [
+          DottedBorder(
+            child: SizedBox(
+              height: screenHeight / 5,
+              width: double.infinity,
+              child: const Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.folder_open,
+                    size: 40,
+                  ),
+                  Gap(15),
+                  Text(
+                    'Select the thumbnail for your song',
+                    style: TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
